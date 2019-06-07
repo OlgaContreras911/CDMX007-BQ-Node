044e283cc584b1386e17131c7a0f898c0b26bbdc
OlgaContreras911 <rockersita.911@gmail.com>
2019-06-06 19:50:04 -0500

    agregando segundo producto a BD

--- index.js
+++ index.js
@@ -1,30 +1,42 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const config = require('./config');
-const authMiddleware = require('./middleware/auth');
-const errorHandler = require('./middleware/error');
-const routes = require('./routes');
-const pkg = require('./package.json');
-
+const express = require("express");
+const mongoose = require("mongoose");
+const config = require("./config");
+const authMiddleware = require("./middleware/auth");
+const errorHandler = require("./middleware/error");
+const routes = require("./routes");
+const pkg = require("./package.json");
 
 const { port, mongoUrl, secret } = config;
 const app = express();
 
-
 // Conectar aplicación a MongoDB
-mongoose.connect(mongoUrl, { useNewUrlParser: true });
+mongoose.connect(
+  mongoUrl,
+  { useNewUrlParser: true, useCreateIndex: true },
+  err => {
+    if (err) {
+      return err;
+    }
+    console.log("conectado con Mongo");
+  }
+);
 
-
-app.set('config', config);
-app.set('pkg', pkg);
-
+app.set("config", config);
+app.set("pkg", pkg);
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(authMiddleware(secret));
 
-
+const { Burgers } = require("./models/Burgers.js");
 // Registrar rutas
-routes(app, (err) => {
+app.get("/Burgers", (req, res) => {
+  Burgers.find({}, (err, Burgers) => {
+    res.send(Burgers);
+  });
+});
+
+routes(app, err => {
   if (err) {
     throw err;
   }
